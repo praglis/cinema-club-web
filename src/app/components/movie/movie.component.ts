7ac9074d94b1697a2d20106b18f524a155c5609c
Kacper Barszczewski <namishis2@gmail.com>
2019-11-27 20:01:18 +0100

    Home page and simple movie component

---
+++ src/app/components/movie/movie.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/movie.service';
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent implements OnInit {
+
+  movie: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MovieService
+  ) { }
+
+  ngOnInit() {
+    //Do usunięcia gdy będzie działać metoda getMovie/ chwilowe obejście
+    if (window.history.state != null) {
+      this.movie = window.history.state;
+      this.movie.poster_path = this.movie.poster_path.changingThisBreaksApplicationSecurity;
+    }
+    //
+
+    console.log(this.movie);
+    const id = this.route.snapshot.paramMap.get('nick');
+    this.movieService.getMovie(id)
+      .subscribe(data => {
+          //TODO: this.movie = data
+      });
+  }
+
+}
