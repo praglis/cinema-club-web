6f80b314837f478691ebd40853b6f23999546663
VukoDyas <panszwedek@outlook.com>
2020-03-31 23:40:34 +0200

    displaying data of logged user

--- src/app/components/my-profile/my-profile.component.ts
+++ src/app/components/my-profile/my-profile.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Injectable, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import { User } from '../../interfaces/user.interface';
+import { Address } from '../../interfaces/address.interface';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-my-profile',
@@ -23,36 +25,32 @@ export class MyProfileComponent implements OnInit {
   @Input() birthdayDate: Date;
   @Input() email: string;
   @Input() phoneNo: string;
-  @Input() address: string;
+  @Input() address: Address;
+
 
   constructor(private userService: UserService) {
-    const userLocalStorage = JSON.parse(localStorage.getItem('currentUser'));
-    this.username = userLocalStorage.username;
-    const user = userService.findByUsername(this.username);
-    console.log("user: ", (user as unknown as User));
-    this.id = (user as unknown as User).id;
-    this.enrolmentDate = (user as unknown as User).enrolmentDate;
-    this.points = (user as unknown as User).points;
-    this.name = (user as unknown as User).name;
-    this.surname = (user as unknown as User).surname;
-    this.birthdayDate = (user as unknown as User).birthday;
-    this.email = (user as unknown as User).email;
-    this.phoneNo = (user as unknown as User).phoneNo;
-    this.address = (user as unknown as User).address;
   }
 
   ngOnInit() {
-    // this.userService.findByUsername(this.username).subscribe((jsonObject: User) => {
-    //   this.id = (jsonObject as User).id;
-    //   this.username = (jsonObject as User).username;
-    //   this.enrolmentDate = (jsonObject as User).enrolmentDate;
-    //   this.points = (jsonObject as User).points;
-    //   this.name = (jsonObject as User).name;
-    //   this.surname = (jsonObject as User).surname;
-    //   this.birthdayDate = (jsonObject as User).birthday;
-    //   this.email = (jsonObject as User).email;
-    //   this.phoneNo = (jsonObject as User).phoneNo;
-    //   this.address = (jsonObject as User).address;
-    // });
+    this.userService.findLoggedUser().subscribe((jsonObject: User) => {
+      this.id = (jsonObject as User).id;
+      this.username = (jsonObject as User).username;
+      this.enrolmentDate = (jsonObject as User).enrolmentDate;
+      this.points = (jsonObject as User).points;
+      this.name = (jsonObject as User).name;
+      this.surname = (jsonObject as User).surname;
+      this.birthdayDate = (jsonObject as User).birthday;
+      this.email = (jsonObject as User).email;
+      this.phoneNo = (jsonObject as User).phoneNo;
+      this.address = (jsonObject as User).address as unknown as Address;
+    });
+  }
+
+  getCleanAddress(): string {
+    console.log('address:', this.address);
+
+    const cleanAddress = this.address.streetName + ' ' + this.address.houseNumber + ', '
+    + this.address.city + ', ' + this.address.state + ', ' + this.address.country;
+    return cleanAddress;
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
-  url = 'http://localhost:8200/user';
+
 
   constructor(
     private httpClient: HttpClient
@@ -18,14 +18,19 @@ export class UserService {
       .pipe();
   }
 
-  findByUsername(username: string): Observable<any> {
-    // const url = 'http://localhost:8200/user/get?username=' + username;
-    return this.httpClient.get<any>(this.url)
-      .pipe();
+  // findLoggedUser(): Observable<any> {
+  //   const url = 'http://localhost:8200/user';
+  //   return this.httpClient.get<any>(url)
+  //     .pipe();
+  // }
+
+  findLoggedUser(): Observable<any> {
+    const url = 'http://localhost:8200/user';
+    return this.httpClient.get<any>(url,  {withCredentials: true}).pipe();
   }
 
   getAllUsers(): Observable<any>{
-    let url = "http://localhost:8200/getUsers";
+    let url = 'http://localhost:8200/getUsers';
     return this.httpClient.get<any>(url, {withCredentials : true})
       .pipe();
   }
