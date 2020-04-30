7dfd672b410267bec1cbfcbd98564233ef2f24bb
ernest <erneststachelski@gmail.com>
2020-04-30 15:08:28 +0200

    CC-124 show badges

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -45,6 +45,7 @@ export class MovieComponent implements OnInit, AfterViewInit {
   showRateForm = false;
   comments: any = [];
   success_msg: string;
+  badgeName: string;
   error: string;
   isMovieInFavourites: boolean;
   isMovieInPlanToWatch: boolean;
@@ -95,6 +96,10 @@ export class MovieComponent implements OnInit, AfterViewInit {
             break;
           }
         }
+      });
+
+      this.userService.getUserBadge().subscribe( (data) => {
+        this.badgeName = data.name;
       });
 
       this.movieService.getMovieGuardianReview(this.movie.title).subscribe((jsonObject: GuardianResponse) => {
@@ -212,7 +217,7 @@ export class MovieComponent implements OnInit, AfterViewInit {
   submitRate(rate: number) {
     this.movieService.postRate(
       Number(this.route.snapshot.paramMap.get('id')),
-      { rate: rate }).subscribe((data) => {
+      { rate }).subscribe((data) => {
         this.success_msg = 'Rate has been added';
         this.showRateForm = false;
         const id = +this.route.snapshot.paramMap.get('id');
