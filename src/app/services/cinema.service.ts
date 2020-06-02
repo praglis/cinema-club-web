8fd2c2bc601ae3703bed09086f98d3abe3a9dd9c
grozm <grozmyslowska@gmail.com>
2020-06-02 21:13:09 +0200

    CC-123 refactor

--- src/app/services/cinema.service.ts
+++ src/app/services/cinema.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { CinemaInterface } from '../interfaces/cinema.interface';
-import { RateInterface } from '../interfaces/rate.interface';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {CinemaInterface} from '../interfaces/cinema.interface';
+import {RateInterface} from '../interfaces/rate.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,32 +10,37 @@ import { RateInterface } from '../interfaces/rate.interface';
 export class CinemaService {
   constructor(
     private httpClient: HttpClient
-  ) { }
+  ) {
+  }
 
   getCinema(cinemaQuery: any, orderBy: string): Observable<any> {
     let url = 'http://localhost:8200/cinema/find?';
 
     Object.keys(cinemaQuery).forEach((key: string) => {
-      if (cinemaQuery[key] !== undefined) { url += key + '=' + cinemaQuery[key] + '&'; }
+      if (cinemaQuery[key] !== undefined) {
+        url += key + '=' + cinemaQuery[key] + '&';
+      }
     });
     if (orderBy !== undefined) {
-      url += "orderBy=" + orderBy;
+      url += 'orderBy=' + orderBy;
     }
 
-    if (url[-1] === '&') { url = url.substring(0, url.length - 1); }
+    if (url[-1] === '&') {
+      url = url.substring(0, url.length - 1);
+    }
 
-    return this.httpClient.get<CinemaInterface[]>(url, { withCredentials: true })
+    return this.httpClient.get<CinemaInterface[]>(url, {withCredentials: true})
       .pipe();
   }
 
   postRate(id: number, rate: RateInterface): Observable<any> {
     let url = 'http://localhost:8200/cinema/' + id + '/rate';
-    return this.httpClient.post<any>(url, rate, { withCredentials: true }).pipe();
+    return this.httpClient.post<any>(url, rate, {withCredentials: true}).pipe();
   }
 
   getCinemaById(cinemaId: number): Observable<CinemaInterface> {
     let url = 'http://localhost:8200/cinema/' + cinemaId;
-    return this.httpClient.get<CinemaInterface>(url, { withCredentials: true })
+    return this.httpClient.get<CinemaInterface>(url, {withCredentials: true})
       .pipe();
   }
 
